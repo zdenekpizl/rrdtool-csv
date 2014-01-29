e1a93056c4969b95ad942746921e824d8c22ea80
Zdenek Pizl <zdenek.pizl@gooddata.com>
2014-01-29 22:30:54 +0100

    zpi-home joke

--- rrdtoolcsv/rrdtool-csv.py
+++ rrdtoolcsv/rrdtool-csv.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 """A simple wrapper over `rrdtool fetch` to convert Munin data from rrd databases to csv format."""
 
@@ -13,7 +13,16 @@ import logging.handlers
 
 from rrdtoolcsv import rrdtoolcsv_settings
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+syslog = logging.handlers.SysLogHandler(address='/dev/log')
+formatter = logging.Formatter('RRDTOOL-CSV: %(levelname)s %(message)s')
+syslog.setFormatter(formatter)
+logger.addHandler(syslog)
+
+
 def check_output(*popenargs, **kwargs):
+    """ in case there is no subprocess.check_output function, define ours one. No such method prior python-2.7"""
     process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
     output, unused_err = process.communicate()
     retcode = process.poll()
@@ -31,13 +40,20 @@ def parse_args():
     """Parse command-line args"""
     parser = argparse.ArgumentParser(
         description='Converts data from RRD databases to CSV files.')
-    parser.add_argument('-d', '--rrd_dir', type=str, default=rrdtoolcsv_settings.RRDIR, help='Directory with .rrd files')
-    parser.add_argument('-c', '--csv_file', type=str, default=rrdtoolcsv_settings.CSV_FILE, help='.csv file to store results')
-    parser.add_argument('-j', '--json_file', type=str, default=rrdtoolcsv_settings.JSON_FILE, help='.json file with data filters')
-    parser.add_argument('-r', '--resolution', type=int, default=rrdtoolcsv_settings.RESOLUTION,help='fetch resolution in seconds')
-    parser.add_argument('-s', '--start', type=str,default=rrdtoolcsv_settings.START,help='start date in rrd fetch format')
-    parser.add_argument('-e', '--end', type=str,default=rrdtoolcsv_settings.END,help='end date in rrd fetch format')
-    parser.add_argument('--daemon', type=str,default=rrdtoolcsv_settings.DAEMON,help='rrdcached daemon')
+    parser.add_argument('-d', '--rrd_dir', type=str, default=rrdtoolcsv_settings.RRDIR,
+                        help='Directory with .rrd files')
+    parser.add_argument('-c', '--csv_file', type=str, default=rrdtoolcsv_settings.CSV_FILE,
+                        help='.csv file to store results')
+    parser.add_argument('-j', '--json_file', type=str, default=rrdtoolcsv_settings.JSON_FILE,
+                        help='.json file with data filters')
+    parser.add_argument('-r', '--resolution', type=int, default=rrdtoolcsv_settings.RESOLUTION,
+                        help='fetch resolution in seconds')
+    parser.add_argument('-s', '--start', type=str, default=rrdtoolcsv_settings.START,
+                        help='start date in rrd fetch format')
+    parser.add_argument('-e', '--end', type=str, default=rrdtoolcsv_settings.END,
+                        help='end date in rrd fetch format')
+    parser.add_argument('--daemon', type=str, default=rrdtoolcsv_settings.DAEMON,
+                        help='rrdcached daemon')
     return parser.parse_args()
 
 
@@ -50,49 +66,50 @@ def run():
     logger.info("args.end: %s \n" % args.end)
 
     rrdtool_output = {}
-    files = files_to_read(args.json_file,args.rrd_dir)
+    files = files_to_read(args.json_file, args.rrd_dir)
 
-    try: subprocess.check_output
-    except: subprocess.check_output = check_output
+    if not "check_output" in dir(subprocess):
+        subprocess.check_output = check_output
 
     #Read rrd files with `rrdtool fetch` and store outputs in a dictionary
-    for file in files:
-        file_path = os.path.join(args.rrd_dir, file.name)
+    for fname in files:
+        file_path = os.path.join(args.rrd_dir, fname.name)
 
-        # use recorder last timestamp
-        if str(args.start).lower() == "last" || str(args.start).lower() == 'l':
+        # use last timestamp recorded into helper file
+        if str(args.start).lower() == "last" or str(args.start).lower() == 'l':
+            lastfile = args.csv_file + ".timestamp"
             try:
-                lastfile = args.csv_file + ".timestamp"
-                f = os.open(lastfile, os.O_RDONLY)
-                timestamp = f.readline()
+                fd = os.open(lastfile, os.O_RDONLY)
+                timestamp = fd.readline()
                 args.start = timestamp
-                os.close(f)
+                os.close(fd)
             except OSError:
                 args.start = rrdtoolcsv_settings.START
-                logger.critical("Cannot get lasttimestamp from %s for rrdtool file %s\n" %(lastfile, file_path))
+                logger.critical("Cannot get lasttimestamp from %s for rrdtool file %s\n" % (lastfile, file_path))
 
-        call = ['rrdtool', 'fetch', file_path, file.aggregation,
+        call = ['rrdtool', 'fetch', file_path, fname.aggregation,
                 '-r', str(args.resolution),
                 '-s', str(args.start), '-e', str(args.end)]
         if args.daemon:
-            call.append(['--daemon',args.daemon])
-        rrdtool_output[file.get_alias()] = subprocess.check_output(call)
+            call.append(['--daemon', args.daemon])
+        rrdtool_output[fname.get_alias()] = subprocess.check_output(call)
 
     #Merge results in a dictionary keyed by timestamps and dump them to csv file
-    lasttimestamp = dump(merge(rrdtool_output),args.csv_file)
+    lasttimestamp = dump(merge(rrdtool_output), args.csv_file)
     # write last exported timestamp to args.csv_file.timestamp file
+    lastfile = args.csv_file + ".timestamp"
     try:
-        lastfile = args.csv_file + ".timestamp"
-        f = os.open(lastfile, os.O_CREAT | os.O_WRONLY | os.O_EXCL)
-        os.write(f, "%s\n" % lasttimestamp)
-        os.close(f)
+        fd = os.open(lastfile, os.O_CREAT | os.O_WRONLY | os.O_EXCL)
+        os.write(fd, "%s\n" % lasttimestamp)
+        os.close(fd)
     except OSError:
-        logger.critical("Failed to save last exported timestamp to %s (rrdtool file %s)\n" %(lockfile,file_path))
+        logger.critical("Failed to save last exported timestamp to %s (rrdtool file %s)\n" % (lastfile, file_path))
 
 
-def files_to_read(json_file,rrd_dir):
+def files_to_read(json_file, rrd_dir):
     """
-    Return a list of file names to read and convert to CSV. The list is generated based on the parameters in settings file.
+    Return a list of file names to read and convert to CSV. The list is generated based on the parameters
+    in settings file.
 
     @param str json_file: path to the file with data filters
     @param str rrd_dir: path to the directory with rrd files
@@ -101,14 +118,15 @@ def files_to_read(json_file,rrd_dir):
     file_pattern = re.compile(rrdtoolcsv_settings.FILE_FORMAT)
     all_files = os.listdir(rrd_dir)
     data = {}
-    with open(json_file,'r') as f:
+    with open(json_file, 'r') as f:
         data = json.load(f)
     matched_files = []
     for file_name in all_files:
-        match = file_matches(file_pattern,data, file_name)
+        match = file_matches(file_pattern, data, file_name)
         if match:
             matched_files.append(match)
     return matched_files
+
 
 def file_matches(file_pattern, data, file_name):
     """
@@ -119,7 +137,7 @@ def file_matches(file_pattern, data, file_name):
     @param str,unicode file_name: file name to test
     @return MatchedFile if file should be processed, None otherwise
     """
-    match = re.match(file_pattern,file_name)
+    match = re.match(file_pattern, file_name)
     if match:
         matched_file = MatchedFile(file_name)
         matched_file.machine = match.group('machine')
@@ -147,7 +165,6 @@ def file_matches(file_pattern, data, file_name):
     return None
 
 
-
 def merge(rrdtool_output):
     """
     Merge data from raw rrdtool output to a dictionary keyed by timestamp
@@ -157,23 +174,24 @@ def merge(rrdtool_output):
      Each value is a dictionary mapping file alias to the value at given timestamp.
     """
     result = dict()
-    for alias,output in rrdtool_output.iteritems():
+    for alias, output in rrdtool_output.iteritems():
         #Convert raw output to list of rows omitting empty rows
         rows = output.split('\n')[2:-1]
 
         #Parse rows and add values to the result
         for row in rows:
 
-            split = row.split(': ',1)
+            split = row.split(': ', 1)
             timestamp = split[0]
-            value = split[1].replace(',','.')
+            value = split[1].replace(',', '.')
 
-            ts_values = result.setdefault(timestamp,dict())
+            ts_values = result.setdefault(timestamp, dict())
             ts_values[alias] = float(value)
 
     return result
 
-def dump(merged_results,csv_file):
+
+def dump(merged_results, csv_file):
     """
     Dump results of merge function to csv file.
     @param dict merged_results: output of merge() function
@@ -187,7 +205,7 @@ def dump(merged_results,csv_file):
 
     #generate and write rows
     headers = list(headers)
-    headers.insert(0,'timestamp')
+    headers.insert(0, 'timestamp')
 
     with open(csv_file, 'wb') as f:
         writer = csv.writer(f)
@@ -210,7 +228,7 @@ def dump(merged_results,csv_file):
 
 class MatchedFile():
 
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
         self.machine = ''
         self.chart = ''
@@ -224,14 +242,6 @@ class MatchedFile():
         else:
             return self.alias
 
+
 if __name__ == '__main__':
-    global logger
-
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    syslog = logging.handlers.SysLogHandler(address='/dev/log')
-    formatter = logging.Formatter('RRDTOOL-CSV: %(levelname)s %(message)s')
-    syslog.setFormatter(formatter)
-    logger.addHandler(syslog)
-
     run()
